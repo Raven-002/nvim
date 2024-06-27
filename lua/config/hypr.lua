ff6139a2b50c496f747e97f0b2d87137a94e2df8
Raven <152022920+Raven-002@users.noreply.github.com>
2024-06-27 09:04:09 +0300

    Add hyprlang support

---
+++ lua/config/hypr.lua
@@ -0,0 +1,17 @@
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+		pattern = {"*.hl", "hypr*.conf"},
+		callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						cmd = {"hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+		end
+})
+
+-- Hyprlang TreeSitter
+vim.filetype.add({
+  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+})
