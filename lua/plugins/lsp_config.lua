458375bdef89c176785285ccf9406ac01bc81589
Raven <152022920+Raven-002@users.noreply.github.com>
2026-01-23 17:13:06 +0200

    Refactor LSP configuration to use handlers for server setup

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,37 +1,42 @@
 require('mason-lspconfig').setup({
-  ensure_installed = { "lua_ls", "clangd", "cmake" }
+  ensure_installed = { "lua_ls", "clangd", "cmake" },
+  handlers = {
+    function (server_name)
+      vim.lsp.config(server_name, {})
+    end,
+    lua_ls = function ()
+      vim.lsp.config('lua_ls', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'},
+            },
+          },
+          workspace = {
+            library = {
+              [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+              [vim.fn.stdpath "config" .. "/lua"] = true,
+            },
+          },
+        },
+      })
+    end,
+    clangd = function ()
+      vim.lsp.config('clangd', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        on_new_config = function(new_config, _)
+          local status, cmake = pcall(require, "cmake-tools")
+          if status then
+            cmake.clangd_on_new_config(new_config)
+          end
+        end,
+      })
+    end,
+    cmake = function ()
+      vim.lsp.config('cmake', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      })
+    end,
+  }
 })
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Setup servers manually
-require('lspconfig').lua_ls.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {'vim'},
-      },
-    },
-    workspace = {
-      library = {
-        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-        [vim.fn.stdpath "config" .. "/lua"] = true,
-      },
-    },
-  },
-}
-
-require('lspconfig').clangd.setup{
-  capabilities = capabilities,
-  on_new_config = function(new_config, _)
-    local status, cmake = pcall(require, "cmake-tools")
-    if status then
-      cmake.clangd_on_new_config(new_config)
-    end
-  end,
-}
-
-require('lspconfig').cmake.setup{
-  capabilities = capabilities,
-}
