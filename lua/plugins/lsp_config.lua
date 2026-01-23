b6e8e8eb92d27b07de172d6c40ab98260865ff6f
Raven <152022920+Raven-002@users.noreply.github.com>
2026-01-23 17:10:10 +0200

    Refactor LSP configuration and update indent-blankline setup

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -4,36 +4,34 @@ require('mason-lspconfig').setup({
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("mason-lspconfig").setup_handlers {
-  function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
-  end,
-  ["clangd"] = function ()
-    require('lspconfig').clangd.setup{
-      on_new_config = function(new_config, _)
-        local status, cmake = pcall(require, "cmake-tools")
-        if status then
-          cmake.clangd_on_new_config(new_config)
-        end
-      end,
-    }
-  end,
-  ["lua_ls"] = function ()
-    require('lspconfig').lua_ls.setup {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = {'vim'},
-          },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.stdpath "config" .. "/lua"] = true,
-          },
-        },
+-- Setup servers manually
+require('lspconfig').lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'},
       },
-    }
+    },
+    workspace = {
+      library = {
+        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+        [vim.fn.stdpath "config" .. "/lua"] = true,
+      },
+    },
+  },
+}
+
+require('lspconfig').clangd.setup{
+  capabilities = capabilities,
+  on_new_config = function(new_config, _)
+    local status, cmake = pcall(require, "cmake-tools")
+    if status then
+      cmake.clangd_on_new_config(new_config)
+    end
   end,
+}
+
+require('lspconfig').cmake.setup{
+  capabilities = capabilities,
 }
